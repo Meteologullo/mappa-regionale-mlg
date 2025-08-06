1e9a8a38e5f22ebfadd8780cc37834d6537c8b19
Meteologullo <angelologullo21@gmail.com>
2025-08-06 18:47:21 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const path = require("path");
+const { exec } = require("child_process");
+
 const app = express();
 const port = 8080;
 
-// Servi i file statici
 app.use(express.static(path.join(__dirname, "..")));
 
 app.listen(port, () => {
-  console.log(`🌐 Server in ascolto sulla porta ${port}`);
+  console.log(`Server in ascolto sulla porta ${port}`);
+  exec("node server/preload.js", (err, stdout, stderr) => {
+    if (err) {
+      console.error("Errore prerender:", stderr);
+    } else {
+      console.log("Prerender completato");
+    }
+  });
 });
-
-// Avvia lo script di prerendering
-require("./preload.js");
