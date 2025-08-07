69fc264b4e3348704a6282bfb5b56d40255a5c2a
Meteologullo <angelologullo21@gmail.com>
2025-08-07 14:39:45 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
+const puppeteer = require("puppeteer");
 
 const app = express();
 const PORT = 8080;
@@ -13,31 +13,30 @@ const server = app.listen(PORT, async () => {
 
   try {
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
     const page = await browser.newPage();
     await page.goto(`http://localhost:${PORT}/mlgmap.html`, {
       waitUntil: "networkidle0",
-      timeout: 0
+      timeout: 0,
     });
 
     const content = await page.content();
 
     if (!fs.existsSync("dist")) fs.mkdirSync("dist");
-    fs.writeFileSync(path.join("dist", "mlgmap.html"), content);
-    fs.writeFileSync(path.join("dist", "index.html"), content);
+    fs.writeFileSync(path.join("dist", "index.html"), content); // output statico
 
     await browser.close();
     console.log("✅ Prerender completato");
 
     server.close(() => {
-      console.log("✅ Server chiuso.");
+      console.log("🔒 Server chiuso.");
       process.exit(0);
     });
-
   } catch (err) {
     console.error("❌ Errore nel prerender:", err);
     server.close(() => process.exit(1));
   }
 });
+
